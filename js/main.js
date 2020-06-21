fe27a7cab7523fd2ac3d24726e32abec5b9bca61
unknown <sk851831@gmail.com>
2020-06-21 18:28:35 +0530

    update 21st June

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
-$(document).ready(function() {
-
-});
+// function goFloor(floor){
+//     document.getElementsByClassName("lift-button active").remove("active")
+//     var floors = document.getElementsByClassName("section");
+//     for (var i = 0; i < floors.length; i++) {
+//         floors[i].className +=" glass-door";
+//     }
+//     bgFloor = document.getElementById("bg"+floor)[0]
+//     bgFloor.getElementById("")
+// }
